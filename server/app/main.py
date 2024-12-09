270373b9baed80303a0d1c51b0528e2e010b1e71
Benjamin Lawani <benjamin.lawani@outlook.com>
2024-12-09 10:49:36 +0000

    Hopeful final prototype

--- server/app/main.py
+++ server/app/main.py
@@ -445,4 +445,4 @@ async def sign_in(request: Request, user_data: UserLogin, db: Session = Depends(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=10000, reload=False)
